808f5d6f716a785765bcad7100bd143ddddfe9ed
dogi <stefan@unterhauser.name>
2017-01-11 02:28:12 -0500

    automagic5

--- auto/magic.sh
+++ auto/magic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo $GIT_TOKEN
+echo $TOKEN
 
 #curl -H "Accept:  application/vnd.github.black-cat-preview+json" -H "Authorization: token xyz" https://api.github.com/repos/open-learning-exchange/open-learning-exchange.github.io/pulls/428/requested_reviewers --data '{"reviewers":["dogi","waryhermit","hikaruit15"]}'
