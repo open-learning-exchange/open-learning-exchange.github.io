e311f69cfffdb5d9c7c9ae2956a75a5d75c36477
dogi <stefan@unterhauser.name>
2017-01-20 10:51:24 -0500

    echos

--- auto/magic.sh
+++ auto/magic.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo dogi
 echo $TOKEN
+echo TRAVIS_OS_NAME
 
 #curl -H "Accept:  application/vnd.github.black-cat-preview+json" -H "Authorization: token xyz" https://api.github.com/repos/open-learning-exchange/open-learning-exchange.github.io/pulls/428/requested_reviewers --data '{"reviewers":["dogi","waryhermit","hikaruit15"]}'
